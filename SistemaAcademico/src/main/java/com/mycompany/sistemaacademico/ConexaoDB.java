c84b56ae92c164653abaaa98c52cda3e3f6e4cf7
evandro <ferraz.evandro@outlook.com>
2022-11-10 13:48:39 -0300

    PSC - 10/11/22

---
+++ SistemaAcademico/src/main/java/com/mycompany/sistemaacademico/ConexaoDB.java
@@ -0,0 +1,24 @@
+package com.mycompany.sistemaacademico;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+public class ConexaoDB {
+    private static String host = "localhost";
+    private static String porta = "3306";
+    private static String db = "db_usuario";
+    private static String usuario = "root";
+    private static String senha = "1234";
+    
+    public static Connection obtemConexao (){
+        try{
+            Connection c = DriverManager.getConnection("jdbc:mysql://" + host + ":" + porta + "/" + db + "?useTimezone=true&serverTimezone=UTC", usuario,senha);
+            return c;
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
+
+
